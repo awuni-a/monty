6d7ebafa63b5aca44483bb53781a41ca82549f61
awuni-a <awuni.alexander@gmail.com>
2022-12-15 11:03:54 +0000

    Fixing stack

--- _stack.c
+++ _stack.c
@@ -1,13 +1,15 @@
 #include "monty.h"
+
 /**
- * f_stack - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_stack(stack_t **head, unsigned int counter)
+ * _stack - function that sets the format of the data to a stack (LIFO).
+ * @stack: double pointer to the head of stack.
+ * @line_number: script line number
+ *
+ * Return: No return.
+ */
+void _stack(stack_t **stack, unsigned int line_number)
 {
-	(void)head;
-	(void)counter;
-	bus.lifi = 0;
+	(void)(stack);
+	(void)(line_number);
+	var.queue = QUEUE;
 }
